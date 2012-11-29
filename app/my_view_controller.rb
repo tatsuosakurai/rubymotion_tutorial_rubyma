4d41f84999b4e50e5a8be4a6578aee4a7437876f
tatsuo.sakurai <tatsuo.sakurai@gmail.com>
2012-11-29 20:53:15 +0900

    Add say 'Hello!'

--- app/my_view_controller.rb
+++ app/my_view_controller.rb
@@ -4,16 +4,21 @@ class MyViewController < UIViewController
 
     view.backgroundColor = UIColor.whiteColor
 
-    label = UILabel.new.tap do |l|
+    @label = UILabel.new.tap do |l|
       l.text = 'foo'
       l.frame =  [[110, 100], [100, 20]]
     end
-    view.addSubview(label)
+    view.addSubview(@label)
 
     button = UIButton.buttonWithType(UIButtonTypeRoundedRect).tap do |b|
       b.setTitle('Say Hello', forState:UIControlStateNormal)
       b.frame = [[110, 300], [100, 60]]
+      b.addTarget(self, action:'say_hello', forControlEvents:UIControlEventTouchUpInside)
     end
     view.addSubview(button)
+  end
+
+  def say_hello
+    @label.text = 'Hello!'
   end
 end
